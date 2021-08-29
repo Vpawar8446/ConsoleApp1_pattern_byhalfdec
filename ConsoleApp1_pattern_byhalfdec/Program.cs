5a55ee5da5ceb19d9126116f3b83bc13f5afce21
niles <niles@LAPTOP-3K24CQ1R>
2021-08-29 23:36:31 +0530

    Add project files.

---
+++ ConsoleApp1_pattern_byhalfdec/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleApp1_pattern_byhalfdec
+{
+    class PatternNo
+    {
+        public void pattern(int num)
+        {
+            for (int i =num; i >= 1; i--)
+            {
+                for (int j =num ; j >= i; j--)
+                {
+
+                    Console.Write(+j);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PatternNo obj = new PatternNo();
+            Console.Write("Enter the any number : ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            obj.pattern(n);
+        }
+    }
+}
+
+
+
+   
